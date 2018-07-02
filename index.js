eb0624e682b38af20db823d27e9a2dff56716ccf
David Anderson <matandersonlv@hotmail.com>
2018-07-02 20:56:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -56,7 +56,7 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
-  if (cardNumber == null) {
+  if (cardNumber == 0) {
     return "Sorry, we don't have a credit card on file for you.";
   } else {
     var totalValue = 0;
